aa82adebc65cc607f18bff38e2dd380d8109843f
Nishant-Sharma26 <nishusharma1817@gmail.com>
2024-11-18 11:10:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="text-3xl font-bold text-green-800">
       Namaste Everyone, let's build netflix-gpt
+      first we will build netflix-sign in page
+      
     </div>
   );
 }
